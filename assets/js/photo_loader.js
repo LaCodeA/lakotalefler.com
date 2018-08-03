5ca0734fa45ced7fbc1cbb5d1a3c0aba4bfdc457
lakota.lefler <lakota.lefler@gmail.com>
2018-08-02 23:15:25 -0400

    Updating images, rday, marcus, gjones

--- assets/js/photo_loader.js
+++ assets/js/photo_loader.js
@@ -1,35 +1,17 @@
 $(document).ready(function() {
 	var photos = [
-		{
-		  name : 'hara_1.jpg'
-		},
-		{
-		  name : 'hernandez_1.jpg'
-		},
-		{
-		  name : 'robins_1.jpg'
-		},
-		{
-		  name : 'rill_1.jpg'
-		},
-		{
-		  name : 'schaefer_1.jpg'
-		},
-		{
-		  name : 'lola_1.jpg'
-		},
-		{
-		  name : 'miller_2.jpg'
-		},
-		{
-		  name : 'york_1.jpg'
-		},
-		{
-		  name : 'light_1.jpg'
-		},
-		{
-		  name : 'hernandez_2.jpg'
-		}
+		{name : 'rick_day_2.jpg'},
+		{name : 'mderricotte_1.jpg'},
+		{name : 'greg_jones_1.jpg'},
+		{name : 'rick_day_1.jpg'},
+		{name : 'alisha_light_1.jpg'},
+		{name : 'greg_jones_2.jpg'},
+		{name : 'rill_1.jpg'},
+		{name : 'hara_1.jpg'},
+		{name : 'hernandez_1.jpg'},
+		{name : 'robins_1.jpg'},
+		{name : 'lola_1.jpg'},
+		{name : 'miller_2.jpg'},
 	];
 
 	var mainElement = $('#main');
